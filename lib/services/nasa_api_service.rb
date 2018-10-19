eaae0f66e40d49c48ca5337decd798407743755e
Bakang <bakang97@gmail.com>
2018-10-19 15:20:11 +0100

    added some tests

--- lib/services/nasa_api_service.rb
+++ lib/services/nasa_api_service.rb
@@ -16,5 +16,79 @@ class NasaService
 
   end
 
+  def check_hash_elements
+     @asteroids["near_earth_objects"]["2015-09-08"].each do |element|
+       unless element.is_a? Hash
+         return false
+       end
+    end
+    return true
+  end
+
+  def check_links_key_class key, klass
+    @asteroids["near_earth_objects"]["2015-09-08"].each do |element|
+      unless element[key].is_a? klass
+        return false
+      end
+    end
+    return true
+  end
+
+  def check_links_key_bool key
+    @asteroids["near_earth_objects"]["2015-09-08"].each do |element|
+      unless !!element[key] == element[key]
+        return false
+      end
+    end
+    return true
+  end
+
+  def check_self_string
+    @asteroids["near_earth_objects"]["2015-09-08"].each do |element|
+      unless element["links"]["self"].is_a? String
+        return false
+      end
+    end
+    return true
+  end
+ # =============  ESITMATED DIAMETER METHODS ===========
+  def check_ed_key_class key, klass
+    @asteroids["near_earth_objects"]["2015-09-08"].each do |element|
+      unless element["estimated_diameter"][key].is_a? klass
+        return false
+      end
+    end
+    return true
+  end
+
+  def check_ed_inner_key_class key1,key2,klass
+    @asteroids["near_earth_objects"]["2015-09-08"].each do |element|
+      unless element["estimated_diameter"][key1][key2].is_a? klass
+        return false
+      end
+    end
+    return true
+  end
+
+  # =============  CLOSE APPROACH DATA METHODS ===========
+   def check_ca_key_class key, klass
+     @asteroids["near_earth_objects"]["2015-09-08"].each do |element|
+       unless element["close_approach_data"].first[key].is_a? klass
+         return false
+       end
+     end
+     return true
+   end
+
+   def check_ca_inner_key_class key1,key2,klass
+     @asteroids["near_earth_objects"]["2015-09-08"].each do |element|
+       unless element["close_approach_data"].first[key1][key2].is_a? klass
+         return false
+       end
+     end
+     return true
+   end
+
+
 
 end
--- spec/nasa_api_service_spec.rb
+++ spec/nasa_api_service_spec.rb
@@ -38,9 +38,132 @@ describe "Nasa Api" do
       expect(@nasa_index.asteroids["near_earth_objects"].length).to eq 2
     end
     it "should return an array for 2015-09-08" do
-      expect(@nasa_index)
+      expect(@nasa_index.asteroids["near_earth_objects"]["2015-09-08"]).to be_a Array
     end
   end
+
+  context "core 2015-09-08 information" do
+    it "should return a hash for the elements" do
+      expect(@nasa_index.check_hash_elements).to eq true
+    end
+    it "should return a Hash for links" do
+      expect(@nasa_index.check_links_key_class "links", Hash).to eq true
+    end
+    it "should return a String in self for links" do
+      expect(@nasa_index.check_self_string).to eq true
+    end
+    it "should return a String for id" do
+      expect(@nasa_index.check_links_key_class "id", String).to eq true
+    end
+    it "should return a String for neo_reference_id" do
+      expect(@nasa_index.check_links_key_class "neo_reference_id", String).to eq true
+    end
+    it "should return a String for name" do
+      expect(@nasa_index.check_links_key_class "name", String).to eq true
+    end
+    it "should return a String for nasa_jpl_url" do
+      expect(@nasa_index.check_links_key_class "nasa_jpl_url", String).to eq true
+    end
+    it "should return a Numeric for absolute_magnitude_h" do
+      expect(@nasa_index.check_links_key_class "absolute_magnitude_h", Numeric).to eq true
+    end
+    it "should return a Hash for estimated_diameter" do
+      expect(@nasa_index.check_links_key_class "estimated_diameter", Hash).to eq true
+    end
+    it "should return a Array for close_approach_data" do
+      expect(@nasa_index.check_links_key_class "close_approach_data", Array).to eq true
+    end
+    it "should return a boolean for is_potentially_hazardous_asteroid" do
+      expect(@nasa_index.check_links_key_bool "is_potentially_hazardous_asteroid").to eq true
+    end
+    it "should return a boolean for is_sentry_object" do
+      expect(@nasa_index.check_links_key_bool "is_sentry_object").to eq true
+    end
+  end
+
+  context "core estimated_diameter diameter information" do
+    it "shuold retrun a Hash for kilometers" do
+      expect(@nasa_index.check_ed_key_class "kilometers", Hash).to be true
+    end
+    it "should return a Numeric for estimated_diameter_min" do
+      expect(@nasa_index.check_ed_inner_key_class "kilometers","estimated_diameter_min", Numeric).to eq true
+    end
+    it "should return a Numeric for estimated_diameter_max" do
+      expect(@nasa_index.check_ed_inner_key_class "kilometers","estimated_diameter_max", Numeric).to eq true
+    end
+
+    it "shuold retrun a Hash for meters" do
+      expect(@nasa_index.check_ed_key_class "meters", Hash).to be true
+    end
+    it "should return a Numeric for estimated_diameter_min" do
+      expect(@nasa_index.check_ed_inner_key_class "meters","estimated_diameter_min", Numeric).to eq true
+    end
+    it "should return a Numeric for estimated_diameter_max" do
+      expect(@nasa_index.check_ed_inner_key_class "meters","estimated_diameter_max", Numeric).to eq true
+    end
+
+    it "shuold retrun a Hash for miles" do
+      expect(@nasa_index.check_ed_key_class "miles", Hash).to be true
+    end
+    it "should return a Numeric for estimated_diameter_min" do
+      expect(@nasa_index.check_ed_inner_key_class "miles","estimated_diameter_min", Numeric).to eq true
+    end
+    it "should return a Numeric for estimated_diameter_max" do
+      expect(@nasa_index.check_ed_inner_key_class "miles","estimated_diameter_max", Numeric).to eq true
+    end
+
+    it "shuold retrun a Hash for feet" do
+      expect(@nasa_index.check_ed_key_class "feet", Hash).to be true
+    end
+    it "should return a Numeric for estimated_diameter_min" do
+      expect(@nasa_index.check_ed_inner_key_class "feet","estimated_diameter_min", Numeric).to eq true
+    end
+    it "should return a Numeric for estimated_diameter_max" do
+      expect(@nasa_index.check_ed_inner_key_class "feet","estimated_diameter_max", Numeric).to eq true
+    end
+  end
+
+  context "core close_approach_data information" do
+    it "should return a String for the close_approach_date" do
+      expect(@nasa_index.check_ca_key_class "close_approach_date", String).to eq true
+    end
+    it "should return a String for the epoch_date_close_approach" do
+      expect(@nasa_index.check_ca_key_class "epoch_date_close_approach", Numeric).to eq true
+    end
+    it "should return a String for the relative_velocity" do
+      expect(@nasa_index.check_ca_key_class "relative_velocity", Hash).to eq true
+    end
+    it "should return a String for kilometers_per_second" do
+      expect(@nasa_index.check_ca_inner_key_class "relative_velocity","kilometers_per_second",String).to eq true
+    end
+    it "should return a String for kilometers_per_hour" do
+      expect(@nasa_index.check_ca_inner_key_class "relative_velocity","kilometers_per_hour",String).to eq true
+    end
+    it "should return a String for miles_per_hour" do
+      expect(@nasa_index.check_ca_inner_key_class "relative_velocity","miles_per_hour",String).to eq true
+    end
+
+    it "should return a String for the miss_distance" do
+      expect(@nasa_index.check_ca_key_class "miss_distance", Hash).to eq true
+    end
+    it "should return a String for astronomical" do
+      expect(@nasa_index.check_ca_inner_key_class "miss_distance","astronomical",String).to eq true
+    end
+    it "should return a String for lunar" do
+      expect(@nasa_index.check_ca_inner_key_class "miss_distance","lunar",String).to eq true
+    end
+    it "should return a String for kilometers" do
+      expect(@nasa_index.check_ca_inner_key_class "miss_distance","kilometers",String).to eq true
+    end
+    it "should return a String for miles" do
+      expect(@nasa_index.check_ca_inner_key_class "miss_distance","miles",String).to eq true
+    end
+
+    it "should return a String for the orbiting_body" do
+      expect(@nasa_index.check_ca_key_class "orbiting_body", String).to eq true
+    end
+  end
+
 
 
 end
